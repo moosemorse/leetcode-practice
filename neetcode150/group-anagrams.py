8802e105a1521d47e23dcfd9e3506e681f4696e3
Alex <aivaylovivanov@gmail.com>
2024-11-24 20:35:42 +0000

    group anagrams sol finished

--- neetcode150/group-anagrams.py
+++ neetcode150/group-anagrams.py
@@ -1,41 +1,46 @@
+from typing import List
+
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-    
-        strs.sort()
-        strs = self.split_list(strs) # Returns list of lists (sorted in length)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        strs = sorted(strs, key=len)
+        strs = self.split_list(strs)  # Returns list of lists (sorted by length)
         result = []
 
-        for s in strs: 
-            temp = []
+        for s in strs:
+            temp = {}  # Store hash_val : list of string elements
             for count, val in enumerate(s):
-                temp.append(val) 
-                if i < len(strs) - 1 and len(strs[i]) != len(strs[i+1]): 
-                    result.append(temp)
-                    temp = []
-                if i == len(strs) - 1: 
-                    result.append(temp)
-    
-    # Used to compare elements 
-    def create_hashtable(self, strs):
-        hm = {} 
-        for s in strs: 
-            if s in hm: 
-                hm[s] += 1
-            else: 
-                hm[s] = 1
-        return hm 
+                hm = self.create_hashtable(''.join(sorted(val)))
+                hm_tuple = tuple(sorted(hm.items()))  # Convert dict to hashable tuple
+                if hm_tuple in temp:
+                    temp[hm_tuple].append(val)
+                else:
+                    temp[hm_tuple] = [val]
+            for key in temp:
+                values = temp[key]
+                result.append(values)
+            print(temp)
+        return result
 
-    # Used to split list based on length of each element
-    def split_list(self, strs): 
+    # Used to create a frequency table for comparison
+    def create_hashtable(self, strs):
+        hm = {}
+        for s in strs:
+            if s in hm:
+                hm[s] += 1
+            else:
+                hm[s] = 1
+        return hm
+
+    # Used to split list based on the length of each element into a list of lists
+    def split_list(self, strs):
         result = []
         temp = []
-        for i in range(len(strs)): 
-            temp.append(strs[i]) 
-            if i < len(strs) - 1 and len(strs[i]) != len(strs[i+1]): 
+        for i in range(len(strs)):
+            temp.append(strs[i])
+            if i < len(strs) - 1 and len(strs[i]) != len(strs[i + 1]):
                 result.append(temp)
                 temp = []
-            if i == len(strs) - 1: 
+            if i == len(strs) - 1:
                 result.append(temp)
 
-                    
-        
+        return result
